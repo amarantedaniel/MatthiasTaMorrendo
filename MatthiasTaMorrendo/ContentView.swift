491dfb87254005e40a372070925c63d8b0443d07
Eduardo Cunha <hey@drgmr.com>
2019-06-04 19:12:42 -0300

    Initial Commit

---
+++ MatthiasTaMorrendo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MatthiasTaMorrendo
+//
+//  Created by Eduardo Cunha on 6/4/19.
+//  Copyright © 2019 Eduardo Cunha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello Matthias!")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
